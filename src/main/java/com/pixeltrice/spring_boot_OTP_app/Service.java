03083069f9509d4f32926e8fcf72816f0652aef4
premkumarr7 <premkumarpandi03@gmail.com>
2024-07-07 18:16:16 +0530

    Removed Twilio account SID from source code

--- src/main/java/com/pixeltrice/spring_boot_OTP_app/Service.java
+++ src/main/java/com/pixeltrice/spring_boot_OTP_app/Service.java
@@ -1,32 +1,30 @@
 package com.pixeltrice.spring_boot_OTP_app;
-//
-//package com.pixeltrice.spring_boot_OTP_app;
-//
-//import com.twilio.Twilio;
-//import com.twilio.rest.api.v2010.account.Message;
-//import com.twilio.type.PhoneNumber;
-//import org.springframework.stereotype.Component;
-//import org.springframework.util.MultiValueMap;
-//
-//@Component
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+
+@Component
 public class Service {
-//
-//    private final String ACCOUNT_SID ="ACf245c70f17c0c6ce60175df06b476d63";
-//
-//    private final String AUTH_TOKEN = "656b7c43a13c68882c4868531b515bf2";
-//
-//    private final String FROM_NUMBER ="+12567871552";
-//
-//    public void send(SmsPojo sms) {
-//        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-//
-//        Message message = Message.creator(new PhoneNumber(sms.getTo()), new PhoneNumber(FROM_NUMBER), sms.getMessage())
-//                .create();
-//        System.out.println("here is my id:"+message.getSid());// Unique resource ID created to manage this transaction
-//
-//    }
-//
-//    public void receive(MultiValueMap<String, String> smscallback) {
-//    }
-//
+
+    private final String ACCOUNT_SID = System.getenv("ACf245c70f17c0c6ce60175df06b476d63");
+
+    private final String AUTH_TOKEN =  System.getenv("656b7c43a13c68882c4868531b515bf2");
+
+    private final String FROM_NUMBER = System.getenv("+12567871552");
+
+    public void send(SmsPojo sms) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+        Message message = Message.creator(new PhoneNumber(sms.getTo()), new PhoneNumber(FROM_NUMBER), sms.getMessage())
+                .create();
+        System.out.println("here is my id:"+message.getSid());// Unique resource ID created to manage this transaction
+
+    }
+
+    public void receive(MultiValueMap<String, String> smscallback) {
+    }
+
 }
